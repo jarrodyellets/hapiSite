be733d41eebfffab95aa236d540b79d19eccb30a
jarrodyellets <jarrod524@gmail.com>
2019-05-07 11:04:31 +0200

    nav component

---
+++ components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <a class='logo' href='/' title='Back to home' />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import "../assets/styles/variables.css";
+
+  .logo {
+    background: url('../static/hapi.svg') no-repeat 0 0;
+    background-size: contain;
+    height: 75px;
+    width: 100px;
+    float: left;
+  }
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'hapi.js',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
 
