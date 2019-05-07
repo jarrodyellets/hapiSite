b205c569c2892bbf594b2a8c924cf21ddac6c35e
jarrodyellets <jarrod524@gmail.com>
2019-05-07 13:18:41 +0200

    start index

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,40 +1,34 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="navbar">
-      <a class="logo" href="/" title="Back to home"/>
-      <ul class="nav-links">
-        <li class="nav-link" title="Tutorials">Tutorials</li>
-        <li class="nav-link" title="API">API</li>
-        <li class="nav-link" title="Plugins">Plugins</li>
-        <li class="nav-link" title="Updates">Updates</li>
-        <li class="nav-link" title="Contribute">Contrubute</li>
-        <li class="nav-link" title="Resources">Resources</li>
-        <li class="nav-link" title="Help">Help</li>
-      </ul>
-    </div>
+  <div class="navbar">
+    <a class="logo" href="/" title="Back to home"/>
+    <ul class="nav-links">
+      <li class="nav-link" title="Tutorials">Tutorials</li>
+      <li class="nav-link" title="API">API</li>
+      <li class="nav-link" title="Plugins">Plugins</li>
+      <li class="nav-link" title="Updates">Updates</li>
+      <li class="nav-link" title="Contribute">Contrubute</li>
+      <li class="nav-link" title="Resources">Resources</li>
+      <li class="nav-link" title="Help">Help</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {};
+
+
 </script>
 
 <style>
 @import "../assets/styles/variables.css";
 
-.nav-wrapper {
-  position: fixed;
-  top: 0;
-  width: 100%;
-  max-width: 1260px;
-  margin: 0 auto;
-}
-
 .navbar {
+  margin: 0 auto;
   padding: 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  background: #fff;
 }
 
 .logo {
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,12 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        hapiSite
-      </h1>
-      <h2 class="subtitle">
-        hapi Site V1
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+    <div class='index-header'>
+      <div class='index-header1'>The Simple, Secure Framework</div>
+      <div class='index-header2'>Develops Trust</div>
+    </div>
+    <div class='index-description'>Build <span class='bold'>powerful</span>, <span class='bold'>scalable applications</span>, with 
+    <span class='bold'>minimal overhead</span> and full <span class='bold'>out-of-the-box</span> functionality - 
+    <span class='underline'>your code, your way </span>
     </div>
   </section>
 </template>
@@ -32,37 +19,39 @@ export default {
     Logo
   }
 }
+
+// let nav = document.querySelector('.navbar');
+// let content = document.querySelector('.container');
+// content.style.marginTop = nav.offsetHeight+'px';
 </script>
 
 <style>
+
 .container {
-  margin: 0 auto;
+  margin: 100px auto 0 auto;
   min-height: 100vh;
+  max-width: 800px;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.index-header {
+  font-size: 2em;
+  font-weight: 700;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.index-description {
+  margin-top: 50px;
+  font-size: 1.5em;
 }
 
-.links {
-  padding-top: 15px;
+.bold {
+  font-weight: 700;
+}
+
+.underline {
+  text-decoration: underline;
 }
 </style>
